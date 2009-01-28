a58be533bddf6882dc4fb5bd9c3de219debc2e33
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-01-28 21:47:17 +0000

    Started work on the dynamic scope variant. - Simplified the runtime caches. - Now I use the new idea of always-mutable multilevel caches to combat disjoint assemblies.

---
+++ trunk/src/composite/org/unseen/guice/composite/scope/DynamicScopes.java
@@ -0,0 +1,14 @@
+package org.unseen.guice.composite.scope;
+
+import java.lang.annotation.Annotation;
+
+import com.google.inject.Scope;
+
+public class DynamicScopes {
+  private DynamicScopes() {
+  }
+  
+  public static Scope dynamicScope(Class<? extends Annotation> tag) {
+    return new DynamicScope(tag);
+  }
+}
