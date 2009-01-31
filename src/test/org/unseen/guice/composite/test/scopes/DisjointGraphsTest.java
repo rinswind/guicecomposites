b60817e8a96886a2fd3875c3dc39a0d9435c4a9e
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-01-31 19:50:59 +0000

    - Better comments in the NestingTest. - Added a DisjointGraphsTest to check lazily created objects of parent scopes.

---
+++ src/test/org/unseen/guice/composite/test/scopes/DisjointGraphsTest.java
@@ -0,0 +1,86 @@
+package org.unseen.guice.composite.test.scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.junit.Test;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.ScopeAnnotation;
+
+import static java.lang.annotation.RetentionPolicy.*;
+import static java.lang.annotation.ElementType.*;
+
+import static org.unseen.guice.composite.scopes.DynamicScopes.*;
+
+import static junit.framework.Assert.*;
+
+public class DisjointGraphsTest {
+  @ScopeAnnotation
+  @Retention(RUNTIME)
+  @Target({TYPE, METHOD})
+  @interface HorizontalScoped {  
+  }
+  
+  @ScopeAnnotation
+  @Retention(RUNTIME)
+  @Target({TYPE, METHOD})
+  @interface VerticalScoped {  
+  }
+  
+  public interface TopFactory {
+    Top create();
+  }
+  
+  public interface CenterFactory {
+    Center create();
+  }
+  
+  @HorizontalScoped
+  public static class Left {
+  }
+  
+  @HorizontalScoped
+  public static class Right {
+  }
+  
+  @HorizontalScoped
+  public static class Center {
+    @Inject TopFactory tops;
+  }
+  
+  @VerticalScoped
+  public static class Top {
+    @Inject Left left;
+    @Inject Right right;
+  }
+  
+  @Test
+  public void testDisjointScopes() {
+    Injector inj = Guice.createInjector(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bindScope(HorizontalScoped.class, scope(HorizontalScoped.class));
+        bindScope(VerticalScoped.class, scope(VerticalScoped.class));
+        
+        bind(CenterFactory.class).toProvider(factory(CenterFactory.class, HorizontalScoped.class));
+        
+        bind(TopFactory.class).toProvider(factory(TopFactory.class, VerticalScoped.class)).in(HorizontalScoped.class);
+      }
+    });
+    
+    CenterFactory fact = inj.getInstance(CenterFactory.class);
+    
+    Center center = fact.create();
+   
+    Top top1 = center.tops.create();
+    Top top2 = center.tops.create();
+    
+    assertTrue(top1 != top2);
+    assertTrue(top1.left == top2.left);
+    assertTrue(top1.right == top2.right);
+  }
+}
