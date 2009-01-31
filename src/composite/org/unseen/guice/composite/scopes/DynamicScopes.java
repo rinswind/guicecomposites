f50fc1794539aa922298a0c9750a21bcdecb7eb5
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-01-31 21:51:55 +0000

    Added rudimentary parameter support.

--- src/composite/org/unseen/guice/composite/scopes/DynamicScopes.java
+++ src/composite/org/unseen/guice/composite/scopes/DynamicScopes.java
@@ -2,6 +2,7 @@ package org.unseen.guice.composite.scopes;
 
 import java.lang.annotation.Annotation;
 
+import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.Scope;
 import com.google.inject.ScopeAnnotation;
@@ -32,6 +33,47 @@ public class DynamicScopes {
     return new DynamicScopeFactoryProvider<F, S>(iface, tag);
   }
   
+  /**
+   * @param <T>
+   * @param key
+   * @return
+   */
+  public static <T> Provider<T> external(Key<T> key) {
+    return new DynamicScopeParameterProvider<T>(key);
+  }
+  
+  /**
+   * @param value
+   * @return
+   */
+  public static Parameter parameter(final String value) {
+    return new Parameter() {
+      public String value() {
+        return value;
+      }
+
+      public Class<? extends Annotation> annotationType() {
+        return Parameter.class;
+      }
+      
+      public String toString() {
+        return "@" + Parameter.class.getName() + "(value=" + value + ")";
+      }
+      
+      public boolean equals(Object o) {
+        return o instanceof Parameter && value.equals(((Parameter) o).value());
+      }
+      
+      public int hashCode() {
+        return (127 * "value".hashCode()) ^ value.hashCode();
+      }
+
+    };
+  }
+  
+  /**
+   * @param tag
+   */
   private static void checkScopeAnnotation(Class<? extends Annotation> tag) {
     if (tag.getAnnotation(ScopeAnnotation.class) == null) {
       throw new IllegalArgumentException(tag + " is not a scope annotation");
