0c22e9ee221080c36ee8fd39eee00dcd7070aeeb
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-02-01 13:05:18 +0000

    - Refactored the dynamic proxy into a method suite build once at configuration time and multiple instances that hold the state and are created at every injection.

--- src/composite/org/unseen/guice/composite/scopes/DynamicScopeFactory.java
+++ src/composite/org/unseen/guice/composite/scopes/DynamicScopeFactory.java
@@ -3,22 +3,16 @@ package org.unseen.guice.composite.scopes;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.HashMap;
 import java.util.Map;
 
-import com.google.inject.ConfigurationException;
 import com.google.inject.Injector;
-import com.google.inject.ProvisionException;
-import com.google.inject.internal.Errors;
-import com.google.inject.internal.ErrorsException;
 
 /**
  * @author Todor Boev
  *
  * @param <F>
  */
-public class DynamicScopeFactory<F> implements InvocationHandler {
+public class DynamicScopeFactory implements InvocationHandler {
   private static final Method TO_STRING;
   private static final Method EQUALS;
   private static final Method HASH_CODE;
@@ -33,62 +27,41 @@ public class DynamicScopeFactory<F> implements InvocationHandler {
     }
   }
   
-  /** Dynamic proxy that can be called to create a new dynamic context */
-  private final F proxy;
-  /** Methods used to create the new contexts */
-  private final Map<Method, DynamicScopeFactoryMethod> methods; 
-  
+  /*
+   * State
+   */
   private final Class<? extends Annotation> scope;
-  private final DynamicContext parent;
+  private final DynamicContext context;
   private final Injector injector;
   
+  /** Method suite */
+  private final Map<Method, DynamicScopeFactoryMethod> methods; 
+  
   /**
-   * @param <F>
-   * @param iface
-   * @param parent
+   * @param scope
+   * @param context
    * @param injector
-   * @return
+   * @param methods
    */
-  public static <F> F get(Class<F> iface, Class<? extends Annotation> scope, DynamicContext parent, Injector injector) {
-    return new DynamicScopeFactory<F>(iface, scope, parent, injector).proxy();
-  }
-  
-  /**
-   * @param iface
-   */
-  private DynamicScopeFactory(Class<F> iface, Class<? extends Annotation> scope, DynamicContext parent, Injector injector) {
+  public DynamicScopeFactory(Class<? extends Annotation> scope, DynamicContext context,
+      Injector injector, Map<Method, DynamicScopeFactoryMethod> methods) {
+    
     this.scope = scope;
-    this.parent = parent;
+    this.context = context;
     this.injector = injector;
-    
-    Errors errors = new Errors();
-    try {
-      this.methods = new HashMap<Method, DynamicScopeFactoryMethod>();
-      
-      /* TODO Also grab methods from superinterfaces */
-      for (Method method : iface.getMethods()) {
-        methods.put(method, new DynamicScopeFactoryMethod(method, errors));
-      }
-    } catch (ErrorsException e) {
-      throw new ConfigurationException(e.getErrors().getMessages());
-    }
-    
-    /*
-     * FIX Can cause trouble under OSGi. The problem here is that this class
-     * loader is different from the class loader that contains the internal
-     * classes we use to support this proxy. We need a class loader bridge that
-     * will delegate the loading of our internal classes to our loader and
-     * everything else to the loader of the factory interface.
-     */
-    this.proxy = iface.cast(Proxy.newProxyInstance(
-        iface.getClassLoader(), new Class[] { iface }, this));
+    this.methods = methods;
   }
   
-  /**
-   * @return
-   */
-  private F proxy() {
-    return proxy;
+  public Class<? extends Annotation> scope() {
+    return scope;
+  }
+  
+  public Injector injector() {
+    return injector;
+  }
+  
+  public DynamicContext context() { 
+    return context; 
   }
   
   /**
@@ -111,35 +84,6 @@ public class DynamicScopeFactory<F> implements InvocationHandler {
     }
     
     /* Factory method - delegate to a specialized hander */
-    try {
-      return methods.get(method).invoke(scope, parent, injector, args);
-    } catch (ProvisionException e) {
-      /* If this is an exception declared by the factory method, throw it as-is */
-      if (e.getErrorMessages().size() == 1) {
-        Throwable cause = e.getErrorMessages().iterator().next().getCause();
-        if (cause != null && canRethrow(method, cause)) {
-          throw cause;
-        }
-      }
-      throw e;
-    }
-  }
-  
-  /**
-   * Returns true if {@code thrown} can be thrown by {@code invoked} without
-   * wrapping.
-   */
-  private static boolean canRethrow(Method invoked, Throwable thrown) {
-    if (thrown instanceof Error || thrown instanceof RuntimeException) {
-      return true;
-    }
-
-    for (Class<?> declared : invoked.getExceptionTypes()) {
-      if (declared.isInstance(thrown)) {
-        return true;
-      }
-    }
-
-    return false;
+    return methods.get(method).invoke(this, args);
   }
 }
--- src/composite/org/unseen/guice/composite/scopes/DynamicScopeFactoryProvider.java
+++ src/composite/org/unseen/guice/composite/scopes/DynamicScopeFactoryProvider.java
@@ -2,11 +2,17 @@ package org.unseen.guice.composite.scopes;
 
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.inject.ConfigurationException;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Provider;
+import com.google.inject.internal.Errors;
+import com.google.inject.internal.ErrorsException;
 import com.google.inject.internal.collect.ImmutableList;
 import com.google.inject.spi.Message;
 
@@ -15,8 +21,14 @@ import com.google.inject.spi.Message;
  * @param <F>
  */
 public class DynamicScopeFactoryProvider<F, S extends Annotation> implements Provider<F> {
+  /** The interface of the factories we create */
   private final Class<F> iface;
-  private final Class<S> tag;
+  /** The method suite of the factories we create */
+  private final Map<Method, DynamicScopeFactoryMethod> methods;
+  
+  /** Part of the state loaded into every created factory */
+  private final Class<S> scope;
+  /** Part of the state loaded into every created factory - injected later */
   private Injector injector;
   
   /**
@@ -25,7 +37,20 @@ public class DynamicScopeFactoryProvider<F, S extends Annotation> implements Pro
    */
   public DynamicScopeFactoryProvider(Class<F> iface, Class<S> scope) {
     this.iface = iface;
-    this.tag = scope;
+    this.scope = scope;
+    
+    /* Build the method suite shared by all factory instances we create */
+    Errors errors = new Errors();
+    try {
+      this.methods = new HashMap<Method, DynamicScopeFactoryMethod>();
+      
+      /* TODO Also grab methods from superinterfaces */
+      for (Method method : iface.getMethods()) {
+        methods.put(method, new DynamicScopeFactoryMethod(method, errors));
+      }
+    } catch (ErrorsException e) {
+      throw new ConfigurationException(e.getErrors().getMessages());
+    }
   }
 
   /**
@@ -53,6 +78,16 @@ public class DynamicScopeFactoryProvider<F, S extends Annotation> implements Pro
     DynamicContext active = DynamicContext.active();
     
     /* Return a factory that will continue the scope creation later on */
-    return DynamicScopeFactory.get(iface, tag, active, injector);
+    DynamicScopeFactory factory = new DynamicScopeFactory(scope, active, injector, methods);
+    
+    /*
+     * FIX Can cause trouble under OSGi. The problem here is that this class
+     * loader is different from the class loader that contains the internal
+     * classes we use to support this proxy. We need a class loader bridge that
+     * will delegate the loading of our internal classes to our loader and
+     * everything else to the loader of the factory interface.
+     */
+    return iface.cast(Proxy.newProxyInstance(
+        iface.getClassLoader(), new Class[] { iface }, factory));
   }
 }
