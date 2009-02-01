1cef031952e5f5c7c710a3bfc961c1e44b33f233
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-02-01 19:07:25 +0000

    - Comment cosmetics. - Replaced IllegalArgument/StateException with the more appropriate CreationException. - Removed commented out System.out.println. Must check what Guice uses for debug. - Added a test suite for all test cases. - Started improvements on the DSL. The ultimate one will be to bind a factory together with all of the method parameters in one go.

---
+++ trunk/src/test/org/unseen/guice/composite/scopes/test/DynamicScopesSuite.java
@@ -0,0 +1,10 @@
+package org.unseen.guice.composite.scopes.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DiamondTest.class, DisjointGraphsTest.class, NestingTest.class, ValidationTest.class})
+public class DynamicScopesSuite {
+}
