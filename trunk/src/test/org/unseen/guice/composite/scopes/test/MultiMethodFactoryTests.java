d402f4586697b0f47028e9b8913b3074e0d2e4a0
rinswind <rinswind@51600d6e-0773-11de-85a1-fd18384f7567>
2009-02-06 16:22:23 +0000

    - Better test for multi-method factories.

--- trunk/src/test/org/unseen/guice/composite/scopes/test/MultiMethodFactoryTests.java
+++ trunk/src/test/org/unseen/guice/composite/scopes/test/MultiMethodFactoryTests.java
@@ -3,10 +3,10 @@ package org.unseen.guice.composite.scopes.test;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static junit.framework.Assert.*;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.net.Socket;
 
 import org.junit.Test;
 import org.unseen.guice.composite.scopes.DynamicScopesModule;
@@ -25,35 +25,69 @@ public class MultiMethodFactoryTests {
   @interface ConnectionScoped {
   }
   
-  interface ConnectionFactory {
-    Connection create(Socket socket);
+  interface BoxFactory {
+    IntegerBox create(Integer num);
     
-    Connection create(Socket socket, @Parameter("setting") String setting);
+    StringBox create(String str);
+    
+    CombinedBox create(Integer num, String str);
   }
   
-  public static class Connection {
-    final Socket socket;
-    final String setting;
+  public static class IntegerBox {
+    final Integer num;
     
     @Inject
-    public Connection(@Parameter Socket socket, @Nullable @Parameter("setting") String setting) {
-      this.socket = socket;
-      this.setting = setting;
+    public IntegerBox(@Nullable @Parameter Integer num) {
+      this.num = num;
     }
   }
   
+  public static class StringBox {
+    final String str;
+    
+    @Inject
+    public StringBox(@Nullable @Parameter String str) {
+      this.str = str;
+    }
+  }
+  
+  public static class CombinedBox {
+    @Inject IntegerBox numBox;
+    @Inject StringBox strBox;
+  }
+  
   @Test 
-  public void multiFactoryMethodsTest() {
+  public void testMultipleProducts() {
     Injector inj = Guice.createInjector(new DynamicScopesModule() {
       @Override
       protected void configure() {
-        bind(ConnectionFactory.class).toDynamicScope(ConnectionScoped.class);
-        bind(Connection.class).in(ConnectionScoped.class);
+        bind(BoxFactory.class).toDynamicScope(ConnectionScoped.class);
+        bind(IntegerBox.class).in(ConnectionScoped.class);
+        bind(StringBox.class).in(ConnectionScoped.class);
+        bind(CombinedBox.class).in(ConnectionScoped.class);
       }
     });
     
-    ConnectionFactory fact = inj.getInstance(ConnectionFactory.class);
-    fact.create(new Socket());
-    fact.create(new Socket(), "test");
+    BoxFactory fact = inj.getInstance(BoxFactory.class);
+    
+    IntegerBox ib = fact.create(42);
+    assertTrue(42 == ib.num);
+    
+    StringBox sb = fact.create("42");
+    assertEquals("42", sb.str);
+    
+    CombinedBox cb = null;
+    
+    cb = fact.create(42, "42");
+    assertTrue(42 == cb.numBox.num);
+    assertEquals("42", cb.strBox.str);
+    
+    cb = fact.create(null, "42");
+    assertNull(cb.numBox.num);
+    assertEquals("42", cb.strBox.str);
+    
+    cb = fact.create(42, null);
+    assertTrue(42 == cb.numBox.num);
+    assertNull(null, cb.strBox.str);
   }
 }
